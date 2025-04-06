4ea4b6791045d760a452ad24690b33d9de424ab6
dilankayasuru <dilankayasuru001@gmail.com>
2025-04-07 00:03:57 +0530

    Format markdown

--- frontend/app/components/chat.tsx
+++ frontend/app/components/chat.tsx
@@ -5,6 +5,7 @@ import { apiGetMessages } from "../api";
 import { useAppDispatch, useAppSelector } from "@/stores/hooks";
 import { setMessages } from "@/stores/messagesSlice";
 import { ScrollToBottom } from "./scrollToBottom";
+import Markdown from "markdown-to-jsx";
 
 export const Chat = () => {
     const messages = useAppSelector((state) => state.messages.value);
@@ -28,11 +29,11 @@ export const Chat = () => {
             {
                 messages.map((message, idx) => (
                     <Fragment key={idx}>
-                        <div className="self-end bg-primary p-4 rounded-2xl w-fit mb-2 max-w-xs">{message.message}</div>
+                        <div className="self-end bg-primary p-4 rounded-2xl w-fit mb-2 max-w-xs break-words"><Markdown>{message.message}</Markdown></div>
                         {message.createdAt &&
-                            <div className="self-end text-sm mb-4">{new Date(message.createdAt).toLocaleTimeString()}</div>
+                            <div className="self-end text-sm mb-4">{new Date(message.createdAt).toLocaleString()}</div>
                         }
-                        <div className={`bg-accent p-4 rounded-2xl mr-8 w-fit max-w-xs mb-4 ${message.reply === "thinking..." ? 'animate-pulse' : null}`}>{message.reply}</div>
+                        <div className={`bg-accent p-4 rounded-2xl mr-8 w-fit max-w-xs mb-4 break-words ${message.reply === "thinking..." ? 'animate-pulse' : null}`}><Markdown>{message.reply}</Markdown></div>
                     </Fragment>
                 ))
             }

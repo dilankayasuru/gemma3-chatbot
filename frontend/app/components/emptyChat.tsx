a72b85f3351b50a9569dcbb05a65a916caeaa078
dilankayasuru <dilankayasuru001@gmail.com>
2025-04-07 13:56:39 +0530

    fix empty chat ui

--- frontend/app/components/emptyChat.tsx
+++ frontend/app/components/emptyChat.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const EmptyChat = () => {
     return (
-        <div className="flex justify-center items-center flex-col">
+        <div className="flex justify-center items-center flex-col mt-72">
             <Image src="/images/logo.webp" alt="Robot logo" width={120} height={120} />
             <p className="text-2xl font-semibold">How can I help you today?</p>
         </div>
